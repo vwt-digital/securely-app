b3e158f34b41afc5e92fae66927a41079aa515b8
Ruben <rubenvanvreeland@gmail.com>
2020-03-04 16:52:21 +0100

    Use unix sed

--- init.sh
+++ init.sh
@@ -6,13 +6,13 @@ fi
 # Auto elastic default password
 (grep -qF "ELASTIC_PASSWORD_PLACEHOLDER" .env) && {
   PASSWORD=`openssl rand -base64 32 | tr -d /=+ | head -c 16`
-  sed -i ''  "s/ELASTIC_PASSWORD_PLACEHOLDER/$PASSWORD/" .env
+  sed -i  "s/ELASTIC_PASSWORD_PLACEHOLDER/$PASSWORD/" .env
   echo "Elastic password is saved in .env"
 }
 
 # Auto kibana encryption key
 (grep -qF "KIBANA_ENCRYPTIONKEY_PLACEHOLDER" .env) && {
-  PASSWORD=`openssl rand -base64 32 | tr -d /=+ | head -c 16`
-  sed -i '' "s/KIBANA_ENCRYPTIONKEY_PLACEHOLDER/$PASSWORD/" .env
+  PASSWORD=`openssl rand -base64 32 | tr -d /=+ | head -c 32`
+  sed -i "s/KIBANA_ENCRYPTIONKEY_PLACEHOLDER/$PASSWORD/" .env
   echo "Kibana encryption key is saved in .env"
 }
